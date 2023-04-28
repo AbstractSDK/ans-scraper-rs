6bf84459b3c9a38ece4e801788ea5b4ba3f1f630
adairrr <32375605+adairrr@users.noreply.github.com>
2023-04-28 09:22:38 +0300

    Initial semi-working script

--- src/bin/astroport.rs
+++ src/bin/astroport.rs
@@ -1,46 +1,43 @@
 use std::collections::{HashMap, HashSet};
-use std::fmt::format;
-use std::path::Path;
-use std::sync::Arc;
 
-use abstract_boot::Abstract;
+use std::path::Path;
+
+
+
 use abstract_core::{
     objects::{
         PoolMetadata,
         PoolType,
-        gov_type::GovernanceDetails,
         pool_id::UncheckedPoolAddress
     },
-    abstract_token::QueryMsgFns as Cw20QueryMsgFns,
 };
 use abstract_core::objects::AssetEntry;
 use abstract_core::objects::oracle::Oracle;
+
 use astroport::{
     asset::AssetInfo,
     factory::{PairsResponse, PairType, QueryMsgFns}
 };
 use clap::Parser;
-use cosmwasm_std::{Addr, StdResult};
+use cosmwasm_std::{Addr};
 use cw20::TokenInfoResponse;
 use cw_asset::AssetInfoUnchecked;
-use cw20_base_orch::msg::{InstantiateMsg as Cw20InstantiateMsg, ExecuteMsg as Cw20ExecuteMsg, QueryMsg as Cw20QueryMsg};
-use cw20_base_orch::state::TokenInfo;
+use cw20_base_orch::msg::{QueryMsg as Cw20QueryMsg};
 
-use cw_orch::{ContractInstance, DaemonBuilder, DaemonChannel, networks::{ChainInfo, NetworkInfo}, Daemon, networks::parse_network, queriers::DaemonQuerier, queriers::Ibc, Contract};
-use cw_orch::queriers::CosmWasm;
+
+use cw_orch::{ContractInstance, networks::{ChainInfo}, Daemon, networks::parse_network, queriers::DaemonQuerier, queriers::Ibc, Contract};
+
 use reqwest::Error;
 use serde::{Deserialize, Serialize};
 use tokio::runtime::Runtime;
 
 use ibc_chain_registry::asset_list::{Asset, AssetList, DenomUnit};
-use ibc_chain_registry::chain::{ChainData, Grpc};
+
 use ibc_chain_registry::constants::ALL_CHAINS;
-use ibc_chain_registry::error::RegistryError;
+
 use ibc_chain_registry::fetchable::Fetchable;
 
 pub const ABSTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-
-#[cw_orch::contract(In)]
 
 #[derive(Serialize, Deserialize, Debug)]
 struct JSONResponse(HashMap<String, String>);
@@ -76,12 +73,12 @@ pub async fn fetch_astroport_address(url: &str, key: &str) -> Result<String, Err
     Ok(key_address.to_string())
 }
 
-const ASTROPORT_PHOENIX_ADDRS: &'static str = "https://raw.githubusercontent.com/astroport-fi/astroport-changelog/main/terra-2/phoenix-1/core_phoenix.json";
-const ASTROPORT_PISCO_ADDRS: &'static str = "https://raw.githubusercontent.com/astroport-fi/astroport-changelog/main/terra-2/pisco-1/core_pisco.json";
+const ASTROPORT_PHOENIX_ADDRS: &str = "https://raw.githubusercontent.com/astroport-fi/astroport-changelog/main/terra-2/phoenix-1/core_phoenix.json";
+const ASTROPORT_PISCO_ADDRS: &str = "https://raw.githubusercontent.com/astroport-fi/astroport-changelog/main/terra-2/pisco-1/core_pisco.json";
 
 const DEX_NAME: &str = "astroport";
 
-fn fetch_asset_lists(rt: Runtime) -> anyhow::Result<Vec<AssetList>> {
+async fn fetch_asset_lists() -> anyhow::Result<Vec<AssetList>> {
     // check for cache dir
     if !Path::new("cache/asset_lists").exists() {
         std::fs::create_dir_all("cache/asset_lists")?;
@@ -99,7 +96,7 @@ fn fetch_asset_lists(rt: Runtime) -> anyhow::Result<Vec<AssetList>> {
             continue;
         }
 
-        let list = rt.block_on(AssetList::fetch(chain.to_string(), None))?;
+        let list = AssetList::fetch(chain.to_string(), None).await.ok().unwrap();
         let json = serde_json::to_string(&list)?;
         std::fs::write(file_name, json)?;
         lists.push(list);
@@ -114,7 +111,7 @@ pub fn astroport_ans(network: ChainInfo) -> anyhow::Result<()> {
     let rt = Runtime::new()?;
 
     let chain = Daemon::builder()
-        .chain(network)
+        .chain(network.clone())
         .handle(rt.handle())
         .build()
         .unwrap();
@@ -125,11 +122,11 @@ pub fn astroport_ans(network: ChainInfo) -> anyhow::Result<()> {
         _ => panic!("Network not supported"),
     };
 
-    let asset_lists = fetch_asset_lists(rt)?;
+    let asset_lists = rt.block_on(fetch_asset_lists())?;
 
-    let ibc = Ibc::new(chain.state.grpc_channel.clone());
+    let _ibc = Ibc::new(chain.state.grpc_channel.clone());
 
-    let url = match (network.chain_id).to_string().as_str() {
+    let url = match network.chain_id {
         "phoenix-1" => ASTROPORT_PHOENIX_ADDRS,
         "pisco-1" => ASTROPORT_PISCO_ADDRS,
         _ => panic!("Network not supported"),
@@ -151,186 +148,134 @@ pub fn astroport_ans(network: ChainInfo) -> anyhow::Result<()> {
             break;
         }
         all_pairs.append(&mut pairs);
-        start_after_pair = all_pairs.last().map(|p| p.asset_infos);
+        start_after_pair = all_pairs.last().map(|p| p.asset_infos.to_vec());
     }
 
-    let all_asset_infos: HashSet<AssetInfo> = all_pairs
-        .into_iter()
-        .flat_map(|p| p.asset_infos.into_iter())
+    let all_asset_infos: HashSet<AssetInfo> = all_pairs.clone()
+        .iter()
+        .flat_map(|p| p.asset_infos.to_vec())
         .collect();
 
     let mut asset_info_to_name: HashMap<AssetInfo, String> = HashMap::new();
 
-    // threshhold for
-    let ans_assets_to_add: Vec<(String, AssetInfoUnchecked)> = all_asset_infos
-        .into_iter()
-        .map(|asset_info| {
-            let (name, unchecked_info) = match asset_info.clone() {
+    let mut not_found_assets = vec![];
 
-                AssetInfo::Token { contract_addr } => {
-                    let entry = rt.block_on(cw20_asset_entry(chain, chain_ans_prefix, &contract_addr))?;
+    let mut ans_assets_to_add = Vec::<(String, AssetInfoUnchecked)>::new();
 
-                    (entry, AssetInfoUnchecked::cw20(contract_addr))
+    for asset_info in all_asset_infos {
+        let (name, unchecked_info) = match &asset_info {
+            AssetInfo::Token { contract_addr } => {
+                continue;
+                if let Ok(entry) = cw20_asset_entry(chain.clone(), chain_ans_prefix, contract_addr) {
+                    (entry, AssetInfoUnchecked::cw20(contract_addr.clone()))
+                } else {
+                    not_found_assets.push(asset_info.clone());
+                    continue;
                 }
-                AssetInfo::NativeToken { denom } => {
-                    let entry = rt.block_on(native_asset_entry(chain, denom.clone(), &asset_lists));
-                    let entry = entry.unwrap_or_else(|| {
-                        panic!("Native asset not found: {}", denom);
-                    });
-                    (entry, AssetInfoUnchecked::native(denom))
-                },
-            };
+            }
+            AssetInfo::NativeToken { denom } => {
+                if let Some(entry) = rt.block_on(native_asset_entry(chain.clone(), denom.clone(), &asset_lists)) {
+                    (entry, AssetInfoUnchecked::native(denom.clone()))
+                } else {
+                    not_found_assets.push(asset_info.clone());
+                    continue;
+                }
+            }
+        };
 
-            asset_info_to_name.insert(asset_info, name.clone());
+        asset_info_to_name.insert(asset_info.clone(), name.clone());
+        ans_assets_to_add.push((name, unchecked_info));
+    }
 
-            Ok((name, unchecked_info))
-        })
-        .collect::<anyhow::Result<Vec<_>>>()?;
+    // println!("Not found assets: {:?}", not_found_assets);
+    println!("To add: {:?}", ans_assets_to_add);
 
-    let ans_pools_to_add: Vec<(UncheckedPoolAddress, PoolMetadata)> = all_pairs
-        .into_iter()
-        .map(|pair| {
-            let pool_id = UncheckedPoolAddress::contract(pair.contract_addr);
+    let mut ans_pools_to_add = Vec::<(UncheckedPoolAddress, PoolMetadata)>::new();
+    let mut skipped_ans_pools = vec![];
 
-            let pool_type = match pair.pair_type {
-                PairType::Stable {} => PoolType::Stable,
-                PairType::Xyk {} => PoolType::ConstantProduct,
-                PairType::Concentrated {} => PoolType::Weighted,
-                PairType::Custom(_) => panic!("Custom pair type not supported"),
-            };
-            let pool_metadata = PoolMetadata {
-                dex: DEX_NAME.to_string(),
-                pool_type,
-                assets: pair
-                    .asset_infos
-                    .into_iter()
-                    .map(|asset_info| {
-                        asset_info_to_name
-                            .get(&asset_info)
-                            .unwrap_or_else(|| panic!("Asset info not found"))
-                            .clone()
-                    })
-                    .map(AssetEntry::from)
-                    .collect::<Vec<_>>(),
-            };
-            (pool_id, pool_metadata)
-        })
-        .collect::<Vec<_>>();
+    for pair in all_pairs {
+        let pool_id = UncheckedPoolAddress::contract(pair.contract_addr);
+
+        let pool_type = match pair.pair_type {
+            PairType::Stable {} => PoolType::Stable,
+            PairType::Xyk {} => PoolType::ConstantProduct,
+            PairType::Concentrated {} => PoolType::Weighted,
+            PairType::Custom(_) => panic!("Custom pair type not supported"),
+        };
+
+        let mut assets = vec![];
+        let mut missing_asset = false;
+
+        for asset_info in &pair.asset_infos {
+            if let Some(name) = asset_info_to_name.get(asset_info) {
+                assets.push(AssetEntry::from(name.clone()));
+            } else {
+                missing_asset = true;
+                break;
+            }
+        }
+
+        if missing_asset {
+            skipped_ans_pools.push(pool_id.clone());
+            continue;
+        }
+
+        let pool_metadata = PoolMetadata {
+            dex: DEX_NAME.to_string(),
+            pool_type,
+            assets,
+        };
+        ans_pools_to_add.push((pool_id, pool_metadata));
+    }
+
 
     Ok(())
 }
 
-async fn cw20_asset_entry(chain: Daemon, chain_ans_prefix: &str, contract_addr: &Addr) -> anyhow::Result<String> {
-    let mut cw20 = Contract::new(contract_addr.clone().as_str(), chain.clone()).with_address(Some(contract_addr));
+fn cw20_asset_entry(chain: Daemon, chain_ans_prefix: &str, contract_addr: &Addr) -> anyhow::Result<String> {
+    let cw20 = Contract::new(contract_addr.clone().as_str(), chain).with_address(Some(contract_addr));
 
     // get the name
     let info: TokenInfoResponse = cw20.query(&Cw20QueryMsg::TokenInfo {})?;
 
-    let name = info.name;
+    let name = info.symbol.to_ascii_lowercase();
     Ok(format!("{}>{}", chain_ans_prefix, name))
 }
 
-/*
-              const denomTrace = await ibcQueryClient.ibc.transfer
-                .denomTrace(denom)
-                .then(({ denomTrace }) => {
-                  if (!denomTrace) {
-                    throw new Error(`No denom trace for ${denom}`)
-                  }
-                  return denomTrace
-                })
-
-              // { path: 'transfer/channel-4', baseDenom: 'uxprt' }
-              const { baseDenom, path } = denomTrace
-
-              // [ 'transfer', 'channel-4' ]
-              const splitPath = path.split('/')
-
-              if (splitPath.length !== 2) {
-                console.log(`Skipping ${denom} because path is not 2 in length: ${path}`)
-                return
-              }
-
-              // ['transfer', 'channel-4']
-              const [portId, channelId] = splitPath
-
-              if (portId !== 'transfer') {
-                console.warn(`Denom trace path for ${denom} is not transfer, but ${portId}`)
-                return
-              }
-
-              // persistence>xprt
-              const ansName = ChainRegistry.externalChainDenomToAnsName(baseDenom)
- */
-
-/*
-  static externalChainDenomToAnsName(searchDenom: string): string {
-    let found: { chain: string; symbol: string } | undefined
-    for (const list of assets) {
-      const { chain_name, assets } = list
-      const foundAsset = assets.find((unit) =>
-        unit.denom_units.some((unit) => unit.denom === searchDenom)
-      )
-      if (foundAsset) {
-        found = { chain: chain_name, symbol: foundAsset.symbol.toLowerCase() }
-        break
-      }
-    }
-    if (!found) {
-      throw new NotFoundError(`asset not found for address ${searchDenom}`)
-    }
-    return AnsName.chainNameIbcAsset(found.chain, found.symbol)
-  }
- */
-async fn native_asset_entry(chain: Daemon, denom: String, x: &Vec<AssetList>) -> Option<String> {
+async fn native_asset_entry(chain: Daemon, denom: String, asset_lists: &[AssetList]) -> Option<String> {
     let ibc = Ibc::new(chain.state.grpc_channel.clone());
 
-    // { path: 'transfer/channel-4', base_denom: 'uxprt' }
     let denom_trace = ibc.denom_trace(denom.clone()).await.ok()?;
 
-    // transfer/channel-4'
+    log::info!("Denom trace for {}: {:?}", denom, denom_trace);
+
     let path = denom_trace.path;
+    let mut path_parts = path.split('/');
 
-    // [ 'transfer', 'channel-4' ]
-    let mut split_path = path.split('/');
-
-    // if split_path.len() != 2 {
-    //     log::warn!("Skipping {} because path is not 2 in length: {}", denom, path);
-    //     return None
-    // }
-
-    // [ 'transfer', 'channel-4' ]
-    let port_id = split_path.next().unwrap();
+    let port_id = path_parts.next().unwrap();
 
     if port_id != "transfer" {
         log::warn!("Denom trace path for {} is not transfer, but {}", denom, port_id);
-        return None
+        return None;
     }
-    // 'xprt'
+
     let base_denom = denom_trace.base_denom;
 
-    let mut ans_asset = None;
+    log::info!("Base denom for {} is {}", denom, base_denom);
 
-    for asset_list in x {
+    for asset_list in asset_lists {
         let AssetList { chain_name, assets, .. } = asset_list;
 
-        let matching_base_denom = assets.iter().find(|asset| {
-            let Asset { symbol, denom_units, .. } = asset;
-
-            denom_units.iter().any(|denom_unit| {
-                let DenomUnit { denom: d, .. } = denom_unit;
-                d == &base_denom
-            })
-
-        });
-
-        if let Some(Asset { symbol, .. }) = matching_base_denom {
-            ans_asset = Some(format!("{}>{}", chain_name, symbol));
-            break;
+        if let Some(matching_asset) = assets.iter().find(|asset| {
+            asset.denom_units.iter().any(|denom_unit| denom_unit.denom == base_denom)
+        }) {
+            return Some(format!("{}>{}", chain_name.to_ascii_lowercase(), matching_asset.symbol.to_ascii_lowercase()));
         }
     }
-    ans_asset
+
+    None
 }
+
 
 #[derive(Parser, Default, Debug)]
 #[command(author, version, about, long_about = None)]
